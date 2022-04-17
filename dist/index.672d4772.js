f29a30ec2b5521e2ec377848bfdfdee8b09962c8
Samandar Qodirqulov <samandarqodirqulov021@gmail.com>
2022-04-17 18:14:25 +0500

    qrwgd

---
+++ dist/index.672d4772.js
@@ -0,0 +1,12 @@
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+}
+let ali = new Person("Samandar");
+let arr = [1, 1, 2];
+Array.find((val) => {
+  return val == 2;
+});
+
+//# sourceMappingURL=index.672d4772.js.map
---
+++ script.js
@@ -0,0 +1,19 @@
+import "lodash-es";
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+const ali = new Person("Samandar");
+
+let arr = [1, 1, 2];
+
+Array.find((val) => {
+  return val == 2;
+});
+
+let arrr = [1, 2, 3];
